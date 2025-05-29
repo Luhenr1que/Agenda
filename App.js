6ea5e87adc8fa7914f9fd52ee6c4ff2b0d2d2383
Luhenr1que <lui.hh.araujo@gmail.com>
2025-05-29 15:16:13 -0300

    29/05

---
+++ App.js
@@ -0,0 +1,52 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+
+import Entrar from './src/login/entrar/index.js';
+import Cadastro from './src/login/cadastro/index.js';
+import Home from './src/home/index.js';
+import Inicio from './src/login/entrar/index.js';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+
+  <NavigationContainer>
+    <StatusBar 
+      hidden={true}
+    />
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Inicio" 
+          component={Inicio} 
+          options={{
+            headerShown: false,
+          }}
+          />
+        <Stack.Screen 
+          name="Cadastro" 
+          component={Cadastro} 
+          options={{
+            headerShown: false,
+          }}
+          />
+        <Stack.Screen 
+          name="Entrar" 
+          component={Entrar} 
+          options={{
+            headerShown: false,
+          }}
+          />
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            headerShown: false,
+          }}
+          />
+        </Stack.Navigator>
+
+  </NavigationContainer>
+)}
+
---
+++ src/login/entrar/index.js
@@ -0,0 +1,10 @@
+import styles from './style.js';
+import { useNavigation } from '@react-navigation/native';
+import { useEffect} from 'react';
+
+export default function Inicio (){
+      const navigation = useNavigation();
+      
+      useEffect(()=>{
+      },)
+}
